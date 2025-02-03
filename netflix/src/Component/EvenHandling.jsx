d04ad6d39a2d70a78ef75360fc0eeca47f25efba
PramodYadav49 <indrashYadav8@gmail.com>
2025-02-03 17:30:24 +0530

    Second Commit

--- netflix/src/Component/EvenHandling.jsx
+++ netflix/src/Component/EvenHandling.jsx
@@ -6,9 +6,17 @@ export const EvenHandling = () => {
         alert("hey I am onClick even")
 
     }
+    function handleWelcome(name){
+        console.log(`hello ${name}`)
+    }
   return (
     <div>
         <button className="border  rounded-sm bg-red-900" onClick={handleButtonClick}> click me</button>
+        <br></br>
+        <button onClick={(event)=>handleButtonClick(event)}>Click me 2</button>
+        <button onClick={(event)=>alert('inline event andler')+console.log(event)}>inline arr fun</button>
+        <br />
+        <button onClick={handleWelcome("Pramod")}>Click pramod</button>
     </div>
   )
 }
